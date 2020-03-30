50a5e8072f1c5271cc63b426c63d9677d9829cfc
BlarCkat <BlarCkat@users.noreply.github.com>
2020-03-31 03:19:30 +0600

    a

--- Pasqo/src/app/app.component.ts
+++ Pasqo/src/app/app.component.ts
@@ -49,24 +49,7 @@ export class AppComponent implements OnInit {
     }
   ];
 
-  onClick(event){
-    let systemDark = window.matchMedia("(prefers-color-scheme: dark)");
-    systemDark.addListener(this.colorTest);
-    if(event.detail.checked){
-      document.body.setAttribute('data-theme', 'dark');
-    }
-    else{
-      document.body.setAttribute('data-theme', 'light');
-    }
-  }
 
-   colorTest(systemInitiatedDark) {
-    if (systemInitiatedDark.matches) {
-      document.body.setAttribute('data-theme', 'dark');		
-    } else {
-      document.body.setAttribute('data-theme', 'light');
-    }
-  }
 
   constructor(
     private platform: Platform,
